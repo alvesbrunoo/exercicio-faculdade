aa72329de2d5c6e03d4946feec0be34ad0e42318
alvesbrunoo <alvesdesouzabruno94@gmail.com>
2026-03-19 18:53:06 -0300

    Create exercicio1.cs

---
+++ exercicio-facul/exercicio1.cs
@@ -0,0 +1,11 @@
+Console.Write("Digite um número: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num >= 10 && num <= 50)
+{
+    Console.WriteLine("Está entre 10 e 50.");
+}
+else
+{
+    Console.WriteLine("Não está entre 10 e 50.");
+}
